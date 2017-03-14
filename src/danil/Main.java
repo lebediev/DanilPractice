47247861c376d8338bf742ddadc169b8d27aee45
alltazar <da_nil@ukr.net>
2017-03-14 19:21:42 +0200

    Update Main.java

--- src/danil/Main.java
+++ src/danil/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main (String[] args){
 
-        List<Integer> uzel1 = Uzel.setSosedi(3);
-        List<Integer> uzel2 = Uzel.setSosedi(3);
-        List<Integer> uzel3 = Uzel.setSosedi(1);
-        List<Integer> uzel0 = Uzel.setSosedi(1,2,3);
+        Uzel uzel0 = new Uzel();
+        Uzel uzel1 = new Uzel();
+        Uzel uzel3 = new Uzel();
+        Uzel uzel2 = new Uzel();
+        uzel0.sosedi = new int[]{1, 2, 3};
+        uzel1.sosedi = new int[]{2};
+        uzel2.sosedi = new int[]{3};
+        uzel3.sosedi = new int[]{1};
+
 
     }
 
-}
