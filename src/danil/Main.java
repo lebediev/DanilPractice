a9cf1fce68acb95a6450220dac65785de248d383
alltazar <da_nil@ukr.net>
2017-03-15 15:04:33 +0200

    Update Main.java

--- src/danil/Main.java
+++ src/danil/Main.java
@@ -1,18 +1,21 @@
-import java.util.List;
+package graf;
 
 public class Main {
 
     public static void main (String[] args){
 
-        Uzel uzel0 = new Uzel();
-        Uzel uzel1 = new Uzel();
-        Uzel uzel3 = new Uzel();
-        Uzel uzel2 = new Uzel();
-        uzel0.sosedi = new int[]{1, 2, 3};
-        uzel1.sosedi = new int[]{2};
-        uzel2.sosedi = new int[]{3};
-        uzel3.sosedi = new int[]{1};
+        Uzel u1 = new Uzel();
+        u1.setToArr(5);
+        u1.setToArr(1);
+        u1.setToArr(7);
+        u1.setToArr(5);
 
+        System.out.println(u1.arr);
+
+        u1.delToArr(5);
+
+        System.out.println(u1.arr);
 
     }
 
+}
